313bad9a6fc37c6c820a04e628d0c072e32dac30
Crys <cryskawaiii@gmail.com>
2023-07-07 12:33:29 -0300

    Alterando título da página

--- api/php.php
+++ api/php.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/styles/style-b.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <title>Com boostrap</title>
+    <title>PHP</title>
 </head>
 <body class="d-flex flex-column" style="background-color: #313131;">
     <header class="text-center p-2 d-flex container-fluid" style="background-color: #8993C1;">
