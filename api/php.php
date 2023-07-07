6dc2bd1cd0806235a240a9006c705cf4af1836c1
Crys <cryskawaiii@gmail.com>
2023-07-07 12:16:26 -0300

    Arquivos do projeto

---
+++ api/php.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/styles/style-b.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Com boostrap</title>
+</head>
+<body class="d-flex flex-column" style="background-color: #313131;">
+    <header class="text-center p-2 d-flex container-fluid" style="background-color: #8993C1;">
+        <a class="align-self-center" href="choose.html"><i class="fa-solid fa-backward p-1"></i></a>
+        <div class="m-auto">
+            <h1 class="display-1 m-auto">The Beginning</h1>
+            <h5 class="py-1 fw-light">Portfólio de Desenvolvimento e Aprendizagem</h5>
+        </div>
+    </header>
+    <main class="flex-grow-1 mx-4 mt-2 position-relative">
+        <div class="row">
+            <div class="botao gy-4 col-6 col-md-3 col-sm-4">
+                <a class="quadrado bg-white text-center rounded-4 d-flex justify-content-center align-items-center" href="assets/primeiros-passos/primeiros-passos-php-estrutural.html">Primeiros Passos PHP</a>
+            </div>
+            <div class="botao gy-4 col-6 col-md-3 col-sm-4">              
+                <a class="quadrado bg-white text-center rounded-4 d-flex justify-content-center align-items-center" href="codes/poo/poo.html">Primeiros Passos POO</a>
+            </div>
+            <div class="botao gy-4 col-6 col-md-3 col-sm-4">
+                <a class="quadrado bg-white text-center rounded-4 d-flex justify-content-center align-items-center" href="https://jogo-da-velha-kappa-dusky.vercel.app/api/index.php" target="_blank">Jogo da Velha</a>
+            </div>
+        </div>
+    </main>
+    <footer class="d-flex justify-content-center">
+        <p>Desenvolvido por Crysnanth</p>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/c1ea196d9b.js" crossorigin="anonymous"></script>
+</body>
+</html>
